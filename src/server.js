906730ce2b3f3076ac2a4201b0588a191517f9e2
plz09 <62223358+plz09@users.noreply.github.com>
2020-06-05 17:39:46 +0100

    Collect point registration logic
    Creating the logic to save the data inserted by the user in the database.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const server = express();
 
+const db = require("./database/db");
+
 server.use(express.static("public"));
+
+//habilitar o uso do req.body
+server.use(express.urlencoded({ extended: true}));
 
 //template engine
 const nunjucks = require("nunjucks");
@@ -15,11 +20,61 @@ server.get("/", (req, res) => {
 });
 
 server.get("/create-point", (req, res) => {
+    //url query strings
+    req.query
+
     return res.render("create-point.html");
 });
 
+server.post("/savepoint", (req, res) => {
+    
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("Cadastrado com sucesso!");
+        console.log(this);
+        return res.send("ok!");
+    }
+    
+    db.run(query, values, afterInsertData);
+})
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html");
+
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if(err) {
+            return console.log(err);
+        }
+
+        const total = rows.length;
+        
+        //Show HTML page with the data from the database
+        return res.render("search-results.html", { places: rows, total: total });
+    });
 });
 
 
