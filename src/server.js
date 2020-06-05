6938296fcd364aaaa6c947b5ceeec4f0997af950
plz09 <62223358+plz09@users.noreply.github.com>
2020-06-05 18:39:00 +0100

    Adjustments

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ server.get("/", (req, res) => {
 
 server.get("/create-point", (req, res) => {
     //url query strings
-    req.query
+    //req.query
 
     return res.render("create-point.html");
 });
@@ -52,12 +52,13 @@ server.post("/savepoint", (req, res) => {
 
     function afterInsertData(err) {
         if(err) {
-            return console.log(err);
+            console.log(err);
+            return res.send("Erro no cadastro!")
         }
 
         console.log("Cadastrado com sucesso!");
         console.log(this);
-        return res.send("ok!");
+        return res.render("create-point.html", {saved: true});
     }
     
     db.run(query, values, afterInsertData);
@@ -65,7 +66,13 @@ server.post("/savepoint", (req, res) => {
 
 server.get("/search", (req, res) => {
 
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    const search = req.query.search;
+
+    if(search == "") {
+        return res.render("search-results.html", { total: 0 }); 
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err);
         }
