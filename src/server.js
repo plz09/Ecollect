36bf8128d012cad9cc377efe31c16688df867447
plz09 <62223358+plz09@users.noreply.github.com>
2020-06-05 02:57:40 +0100

    Template engine configuration

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const server = express();
 
-server.use(express.static("public"))
+server.use(express.static("public"));
+
+//template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
 
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html");
 });
 
 
