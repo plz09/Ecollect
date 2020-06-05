21d30de2cebf7f69aeabdebdff7049524d2e3a5e
plz09 <62223358+plz09@users.noreply.github.com>
2020-06-05 02:45:14 +0100

    Server configuration
    Folder structure and server configuration

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const server = express();
+
+server.use(express.static("public"))
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+
+server.listen(3000);
